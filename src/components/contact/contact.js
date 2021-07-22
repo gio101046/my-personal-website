edf53f407355fd6b24ff3e3f42b0f5c7f0e8963b
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-07-22 19:26:59 -0400

    feat: update contact information

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,10 +10,10 @@ class Contact extends Component {
           <div className="col"> 
             <div className="text-center">
               <h1 className="w-100">
-                Intresting in working with me?
+                Wanna work together?
               </h1>
               <h2 className="mx-auto content-text-size content-width mt-4">
-                You can get a hold of me by texting me at <strong>(305) 570 1332</strong> or you can just email me at <strong>grodriguez@gtek.dev</strong>.
+                You can get a hold of me by emailing me at <strong>me@grodriguez.dev</strong>.
                 You can also see what I am up to by clicking the social media icons below.
               </h2>
             </div>
