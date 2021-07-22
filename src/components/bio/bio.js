c7470c5b8226112afd5750a4500f53b10ce1356e
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-07-22 19:18:00 -0400

    feat: update bio summary

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -10,9 +10,8 @@ class Bio extends Component {
           <div className="col"> 
             <div className="mx-auto text-center border-top border-bottom pl-1 pl-lg-5 pr-1 pr-lg-5 pt-5 pb-5 d-flex h-100 content-width">
               <h2 className="content-text-size">
-                My name is <strong>Giovani Rodriguez</strong> and I create tools, applications and websites as solutions for those that need them. 
-                I am a problem solver, solution maker upper and answer deilverer. I'm always itching to learn something new and strive to deliver my best. When I'm not 
-                creating I am either reading, writing or playing video games. 
+                My name is <strong>Giovani Rodriguez</strong> and I create tools, apps and websites. 
+                I am a problem solver, solution maker upper and answer deilverer. I'm always itching to learn something new and striving to deliver my best. I like to read, listen to music and ride my bike.
               </h2>
             </div>
           </div>
